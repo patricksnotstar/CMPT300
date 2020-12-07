be6255a9c5ed51330e6b59f985d1daefc4bbea12
pna18 <pna18@sfu.ca>
2020-12-06 20:54:59 -0800

    WIP

--- HW4/UnixLs.c
+++ HW4/UnixLs.c
@@ -9,6 +9,8 @@
 #include <time.h>
 #include <unistd.h>
 
+int selectedOptions[3];
+
 void printPermission(mode_t buf)
 {
     printf((buf & S_IRUSR) ? "r" : "-");
@@ -22,13 +24,13 @@ void printPermission(mode_t buf)
     printf((buf & S_IXOTH) ? "x" : "-");
 }
 
-void ls(char *dir, int type)
+void ls(char *dir)
 {
     DIR *dirStream = opendir(dir);
     if (dirStream == NULL)
     {
         printf("Error opening file %s\n", dir);
-        return;
+        exit(1);
     }
 
     struct dirent *dr;
@@ -37,15 +39,19 @@ void ls(char *dir, int type)
     {
         if (dr->d_name[0] != '.')
         {
-            if (type == 1)
+            if (dr->d_type == DT_DIR)
             {
-                printf("%lu  ", dr->d_ino);
+                printf("'%s'  ", dr->d_name);
             }
-            printf("%s  ", dr->d_name);
+            else
+            {
+                printf("%s  ", dr->d_name);
+            }
         }
         dr = readdir(dirStream);
     }
     printf("\n");
+    closedir(dirStream);
 }
 
 void ls_l(char *dir)
@@ -60,35 +66,47 @@ void ls_l(char *dir)
     struct dirent *dr;
     dr = readdir(dirStream);
     struct stat buf;
-    // struct passwd username;
-    // struct group groupname;
     char time[20];
     struct tm *timeinfo;
     char *realPath;
+    int freeFlag = 0;
     while (dr != NULL)
     {
-        stat(dr->d_name, &buf);
+        lstat(dr->d_name, &buf);
         timeinfo = localtime(&buf.st_mtime);
         if (dr->d_name[0] != '.')
         {
-            // if (S_ISLNK(buf.st_mode))
-            // {
-            //     printf((S_ISDIR(buf.st_mode)) ? "l" : "-");
-            //     ssize_t nbytes = readlink(dr->d_name, realPath, buf.st_size + 1);
-            //     realPath = malloc(nbytes + 1);
-            //     realPath[buf.st_size] = '\0';
-            // }
-            // else
-            // {
-            // }
-            printf((S_ISDIR(buf.st_mode)) ? "d" : "-");
-            printPermission(buf.st_mode);
-            printf("%14lu", buf.st_nlink);
-            printf("%14s", getpwuid(buf.st_uid)->pw_name);
-            printf("%14s", getgrgid(buf.st_uid)->gr_name);
-            printf("%14lu", buf.st_size);
-            strftime(time, sizeof(time), "%b %m %y %H:%M", timeinfo);
-            printf("%14s", time);
+            if (selectedOptions[0] == 1)
+            {
+                printf("%lu  ", dr->d_ino);
+            }
+            if (selectedOptions[1] == 1)
+            {
+                if (S_ISLNK(buf.st_mode))
+                {
+                    freeFlag = 1;
+                    printf("l");
+                    realPath = malloc(buf.st_size + 1);
+                    ssize_t nbytes = readlink(dr->d_name, realPath, buf.st_size + 1);
+                    if (nbytes == -1)
+                    {
+                        printf("Error Readlink\n");
+                        exit(1);
+                    }
+                    realPath[buf.st_size] = '\0';
+                }
+                else
+                {
+                    printf((S_ISDIR(buf.st_mode)) ? "d" : "-");
+                }
+                printPermission(buf.st_mode);
+                printf("%14lu", buf.st_nlink);
+                printf("%14s", getpwuid(buf.st_uid)->pw_name);
+                printf("%14s", getgrgid(buf.st_uid)->gr_name);
+                printf("%14lu", buf.st_size);
+                strftime(time, sizeof(time), "%b %m %y %H:%M", timeinfo);
+                printf("%14s", time);
+            }
             if (dr->d_type == DT_DIR)
             {
                 printf("%14s%s'", "'", dr->d_name);
@@ -96,67 +114,111 @@ void ls_l(char *dir)
             else
             {
                 printf("%14s", dr->d_name);
-                // if (S_ISLNK(buf.st_mode))
-                // {
-                //     printf(" -> %s", realPath);
-                // }
+                if (selectedOptions[1] == 1 && S_ISLNK(buf.st_mode))
+                {
+                    printf(" -> %s", realPath);
+                }
             }
             printf("\n");
         }
         dr = readdir(dirStream);
-        // free(realPath);
+        if (freeFlag == 1)
+        {
+            free(realPath);
+            freeFlag = 0;
+        }
     }
+    closedir(dirStream);
+}
+
+void ls_R(char *dir)
+{
+    printf("%s: \n", dir);
+    ls_l(dir);
+    printf("\n");
+    DIR *dirStream = opendir(dir);
+
+    struct dirent *dr;
+    dr = readdir(dirStream);
+    while (dr != NULL)
+    {
+        if (dr->d_name[0] != '.')
+        {
+            if (dr->d_type == DT_DIR)
+            {
+                char *realPath = malloc(sizeof(dir) + sizeof(dr->d_name) + 1);
+                strcpy(realPath, dir);
+                strcat(realPath, "/");
+                strcat(realPath, dr->d_name);
+                ls_R(realPath);
+            }
+        }
+        dr = readdir(dirStream);
+    }
+    closedir(dirStream);
 }
 
 int main(int argc, char *argv[])
 {
+    selectedOptions[0] = 0;
+    selectedOptions[1] = 0;
+    selectedOptions[2] = 0;
     if (argc == 1)
     {
-        ls(".", 0);
-    }
-    else if (argc == 2)
-    {
-        if (strcmp(argv[1], "-i") == 0)
-        {
-            ls(".", 1);
-        }
-        else if (strcmp(argv[1], "-l") == 0)
-        {
-            ls_l(".");
-        }
-        else if (strcmp(argv[1], "-R") == 0)
-        {
-            printf("-R\n");
-        }
-        else
-        {
-            ls(argv[1], 0);
-        }
+        ls(".");
     }
     else
     {
-        if (strcmp(argv[1], "-i") == 0)
+        int lastOptionIdx = 0;
+        for (int i = 1; i < argc; i++)
         {
-            for (int i = 2; i < argc; i++)
+            if (argv[i][0] == '-')
             {
-                printf("%s: \n", argv[i]);
-                ls(argv[i], 1);
+                lastOptionIdx++;
+                for (int j = 1; j < strlen(argv[i]); j++)
+                {
+                    switch (argv[i][j])
+                    {
+                    case 'i':
+                        selectedOptions[0] = 1;
+                        break;
+                    case 'l':
+                        selectedOptions[1] = 1;
+                        break;
+                    case 'R':
+                        selectedOptions[2] = 1;
+                        break;
+                    }
+                }
             }
         }
-        else if (strcmp(argv[1], "-l") == 0)
+
+        if (selectedOptions[2] == 1)
         {
-            printf("-l\n");
-        }
-        else if (strcmp(argv[1], "-R") == 0)
-        {
-            printf("-R\n");
+            if (lastOptionIdx + 1 == argc)
+            {
+                ls_R(".");
+            }
+            else
+            {
+                for (int i = lastOptionIdx + 1; i < argc; i++)
+                {
+                    ls_R(argv[i]);
+                }
+            }
         }
         else
         {
-            for (int i = 1; i < argc; i++)
+            if (lastOptionIdx + 1 == argc)
             {
-                printf("%s: \n", argv[i]);
-                ls(argv[i], 0);
+                ls_l(".");
+            }
+            else
+            {
+                for (int i = lastOptionIdx + 1; i < argc; i++)
+                {
+                    ls_l(argv[i]);
+                }
             }
         }
     }
